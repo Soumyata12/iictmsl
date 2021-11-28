6de9fe00660fb0a5c958a22f0699d9e4b1ea4ef7
shubhayu-64 <majumdarshubhayu@gmail.com>
2021-11-28 11:53:25 +0530

    Initial Commit

---
+++ js/member.js
@@ -0,0 +1,51 @@
+let dict = [
+	{
+		name: 'Dipanjan Guchait',
+		pos: 'Convenor',
+		link: 'https://www.linkedin.com/in/prattay-chakrabarty/',
+		mail: 'prattay2015@gmail.com',
+		photo: './image/coordinators1.png'
+	},
+	{
+		name: 'Priyanshu Kumar',
+		pos: 'Co-Convenor',
+		link: 'https://www.linkedin.com/in/raj-chhetri-74880b148',
+		mail: 'rajchhetriedu@gmail.com',
+		photo: './image/coordinators2.png'
+	},
+	{
+		name: 'Nikhil Agarwal',
+		pos: 'Sponsorship Head',
+		link: 'https://www.linkedin.com/in/sougata-konar-025620154',
+		mail: 'sougatakonar96@gmail.com',
+		photo: './image/coordinators4.png'
+	},
+	{
+		name: 'Ritam Kabiraj',
+		pos: 'Co-Convenor',
+		link: 'https://www.linkedin.com/in/rounak-chourasia/',
+		mail: 'rounak122@gmail.com',
+		photo: './image/coordinators6.jpg'
+	},
+	{
+		name: 'Koustav Banerjee',
+		pos: 'Admin Head & Co-Sponsorship Head',
+		link: 'https://www.linkedin.com/in/amlan-mukherjee',
+		mail: 'amlanph.05@gmail.com',
+		photo: './image/coordinators3.png'
+	},
+	{
+		name: 'Ankit Podder',
+		pos: 'Tech Head',
+		link: 'https://www.linkedin.com/in/sarnav-dutta-786729b5/',
+		mail: 'tanusnyk@gmail.com',
+		photo: './image/coordinators5.png'
+	},
+	{
+		name: 'Moinak Ghosh',
+		pos: 'Resource Head',
+		link: 'https://www.linkedin.com/in/sarnav-dutta-786729b5/',
+		mail: 'rajdeep108pal@gmail.com',
+		photo: './image/coordinators7.jpeg'
+	}
+];
