0cecfd14dc097e07dc2147da5b598c5fffda717e
Aditya Seth <adityaseth.cse@gmail.com>
2022-10-27 20:17:56 +0530

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -179,7 +179,7 @@ if (day === 'enabled') {
 
 nightToggle.addEventListener('click', () => {
   day = localStorage.getItem('day');
-
+<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
   if (day !== 'enabled') {
     enableNightMode();
   } else {
